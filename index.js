4246ecc5188159d60e61729981043ac7bf574634
MlleKan <Kan.dawson@protonmail.com>
2021-11-03 12:56:20 -0400

    commit tree 4dbec14fc265d5ba574241405184664531bb720a

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2 
+let multiply = (num1 * num2)
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+
+const mod = (num3 % num4)
+
+const max = Math.max(5,10,15,20);
+
+
+// getRandomInt(random) {
+//     Number.isInteger(random) > 0
+//     return Math.floor(Math.random() * max);
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+
+
+
+
